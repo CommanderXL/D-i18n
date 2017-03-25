f146f3331bb726761c4feefb28a8ff9de7c2990c
XRene <1017653702@qq.com>
2017-03-26 01:01:39 +0800

    完成字符串的提取

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const fs = require('fs')
 const recursiveDir = require('./recursive-dir')
 const _ = require('./util')
 
+const LANG_DIR = './dist/lang'
+const LANG_FILE = path.join(LANG_DIR, '/index.json')
+
 let files = recursiveDir('./test')
 
+let lang = {}
 files.forEach((item, index) => {
   let data = fs.readFileSync(item, 'utf8')
   let res = _.getComment(data)
 
-  res && console.log(JSON.parse(res[2]))
+  if (res) {
+    lang = Object.assign(lang, JSON.parse(res[2]))
+  }
 })
 
-rm('-rf', './dist/lang')
-mkdir('-p', './dist/lang')
-touch('./dist/lang/index.js')
+rm('-rf', LANG_DIR)
+mkdir('-p', LANG_DIR)
+touch(LANG_FILE)
+
+fs.writeFile(LANG_FILE, JSON.stringify(_.getLangJSON(lang, {})), (err, res) => {
+  if (err) throw err
+  console.log('File has been writed in')
+})
--- util.js
+++ util.js
@@ -1,6 +1,21 @@
 const _ = module.exports
 
-_.getComment = function (str) {
+_.getComment = function (str = '') {
   let pattern = /(\/\*|<!--)\s*<i18n>\s*(\{[\s\S]+\})\s*<i18n>\s*(-->|\*\/)/i
   return str.match(pattern)
+}
+
+_.getLangJSON = function (srcObj = {}, resObj = {}) {
+  let keys = Object.keys(srcObj)
+
+  keys.forEach(key => {
+    let _keys = Object.keys(srcObj[key])
+    _keys.forEach(_key => {
+      !resObj[_key] && (resObj[_key] = {})
+      !resObj[_key][key] && (resObj[_key][key] = '')
+      resObj[_key][key] = srcObj[key][_key]
+    })
+  })
+
+  return resObj
 }
